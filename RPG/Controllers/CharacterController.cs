709cc54f631d390305920d7dc6d3ed63e28b4813
Rafael Santos <rafa.05125@gmail.com>
2023-06-19 08:00:19 -0300

    Add new Character

--- RPG/Controllers/CharacterController.cs
+++ RPG/Controllers/CharacterController.cs
@@ -24,5 +24,12 @@ namespace RPG.Controllers
         {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
     }
 }
